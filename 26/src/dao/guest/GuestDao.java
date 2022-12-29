30ea04c16c4723d1e6a776539a287b47a27db5da
dongpillson <pildong94@naver.com>
2022-12-30 00:10:46 +0900

    mac

--- 26/src/dao/guest/GuestDao.java
+++ 26/src/dao/guest/GuestDao.java
@@ -13,37 +13,84 @@ import java.sql.PreparedStatement;
  */
 public class GuestDao {
 	public int insert(Guest guest) throws Exception {
-		String driverClass="oracle.jdbc.OracleDriver";
-		String url="jdbc:oracle:thin:@182.237.126.19:1521:xe";
-		String user="jdeveloper25";
-		String password="jdeveloper25";
-		
+		String driverClass = "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user = "jdeveloper25";
+		String password = "jdeveloper25";
+
 		String insertSql = "insert into guest values(guest_guest_no_seq.nextval,?,?,?,?,?,?)";
-		
+
 		Class.forName(driverClass);
-		Connection con=DriverManager.getConnection(url, user, password);
+		Connection con = DriverManager.getConnection(url, user, password);
 		PreparedStatement pstmt = con.prepareStatement(insertSql);
-		
-		
+
 		pstmt.setString(1, guest.getGuest_name());
 		pstmt.setDate(2, new java.sql.Date(guest.getGuest_date().getTime()));
 		pstmt.setString(3, guest.getGuest_email());
 		pstmt.setString(4, guest.getGuest_homepage());
 		pstmt.setString(5, guest.getGuest_title());
 		pstmt.setString(6, guest.getGuest_content());
-		
+
 		int rowCount = pstmt.executeUpdate();
 		pstmt.close();
 		con.close();
-		
+
 		return rowCount;
-		
-		
-		
-		
+
 	}
-	public void update() throws Exception {}
-	public void delete() throws Exception {}
-	public void findByPrimaryKey() throws Exception {}
-	public void findAll() throws Exception {}
+
+	public int update(Guest updateGuest) throws Exception {
+		String driverClass = "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user = "jdeveloper25";
+		String password = "jdeveloper25";
+
+		String updateSql = "update guest set guest_no=?, guest_name=?, guest_date=?, guest_email=?, "
+				+ "guest_homepage=?, guest_title=?, guest_content=? where guest_no=?";
+		Class.forName(driverClass);
+		Connection con = DriverManager.getConnection(url, user, password);
+		PreparedStatement pstmt = con.prepareStatement(updateSql);
+
+		pstmt.setInt(1, updateGuest.getGuest_no());
+		pstmt.setString(2, updateGuest.getGuest_name());
+		pstmt.setDate(3, new java.sql.Date(updateGuest.getGuest_date().getTime()));
+		pstmt.setString(4, updateGuest.getGuest_email());
+		pstmt.setString(5, updateGuest.getGuest_homepage());
+		pstmt.setString(6, updateGuest.getGuest_title());
+		pstmt.setString(7, updateGuest.getGuest_content());
+		pstmt.setInt(8, updateGuest.getGuest_no());
+		int rowCount = pstmt.executeUpdate();
+
+		pstmt.close();
+		con.close();
+
+		return rowCount;
+
+	}
+
+	public int delete(int guest_no) throws Exception {
+		String driverClass = "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
+		String user = "jdeveloper25";
+		String password = "jdeveloper25";
+
+		String deleteSql = "delete from guest where guest_no = ?";
+		Class.forName(driverClass);
+		Connection con = DriverManager.getConnection(url, user, password);
+		PreparedStatement pstmt = con.prepareStatement(deleteSql);
+
+		pstmt.setInt(1, guest_no);
+		int rowCount = pstmt.executeUpdate();
+
+		pstmt.close();
+		con.close();
+		return rowCount;
+
+	}
+
+	public void findByPrimaryKey() throws Exception {
+	}
+
+	public void findAll() throws Exception {
+	}
 }
--- 26/src/dao/guest/GuestDaoTestMain.java
+++ 26/src/dao/guest/GuestDaoTestMain.java
@@ -5,20 +5,24 @@ import java.util.Date;
 
 public class GuestDaoTestMain {
 
-	public static void main(String[] args)throws Exception {
-		GuestDao guestDao=new GuestDao();
-		
-		Guest insert = new Guest(0, "dongpil", new SimpleDateFormat("yyyy/MM/dd").parse("2022/12/12"), "pi@nave.com", "www.www", "qmfqk", "dmdmdmdm");
-		guestDao.insert(insert);
-		System.out.println("1.insert");
-		
-		
-		
-		
-		System.out.println("2.delete");
-		System.out.println("3.update");
+	public static void main(String[] args) throws Exception {
+		int rowCount = 0;
+		GuestDao guestDao = new GuestDao();
+
+		Guest guest1 = new Guest(0, "dongpil", new SimpleDateFormat("yyyy/MM/dd").parse("2022/12/12"), "pi@nave.com",
+				"www.www", "qmfqk", "dmdmdmdm");
+		rowCount = guestDao.insert(guest1);
+		System.out.println("1.insert" + rowCount);
+
+		rowCount = guestDao.delete(4);
+		System.out.println("2.delete" + rowCount);
+
+		rowCount = guestDao.update(new Guest(4, "김경호", new SimpleDateFormat("yyyy/MM/dd").parse("2022/12/25"),
+				"naver@naver.com", "zz", "zzz", "zz"));
+		System.out.println("3.update" + rowCount);
+
 		System.out.println("4.findByPrimarykey");
 		System.out.println("5.findAll");
 	}
-	
+
 }
